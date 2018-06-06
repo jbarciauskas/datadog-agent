f756c4e3d334491ea47abda0c7d4e528e53a749b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-06-06 14:44:18 +0200

    [msi] collect root json from right location, attempt to copy_file

--- omnibus/config/software/datadog-agent-integrations.rb
+++ omnibus/config/software/datadog-agent-integrations.rb
@@ -77,7 +77,7 @@ build do
           dest: "#{install_dir}/public-tuf-config.json",
           mode: 0640,
           vars: { tuf_config: tuf_config }
-      copy windows_safe_path("#{project_dir}/\.tuf-root.json"), windows_safe_path("#{install_dir}/etc/datadog-agent/root.json")
+      copy_file windows_safe_path("#{project_dir}/.tuf-root.json"), windows_safe_path("#{install_dir}/etc/datadog-agent/root.json")
     else
       copy windows_safe_path("#{project_dir}/.public-tuf-config.json"), windows_safe_path("#{install_dir}/public-tuf-config.json")
       copy windows_safe_path("#{project_dir}/.tuf-root.json"), windows_safe_path("#{tuf_repo_meta}/current/root.json")
